96bd21e2a683a0509470078fc8712cb4a52779c1
Chely Ho <adept_inept@yahoo.com>
2018-02-13 01:25:42 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -15,8 +15,8 @@ class Transfer
 
   def execute_transaction
     if valid?
-      @sender.balance -= amount
-      @reciever.balance += amount
+      sender.balance -= amount
+      reciever.balance += amount
       self.status = "complete"
     end
   end
