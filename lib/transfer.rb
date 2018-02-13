d0bab87b753ccaf4dd2d20f6ed8c02128c50cb07
Chely Ho <adept_inept@yahoo.com>
2018-02-13 01:18:38 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -3,8 +3,8 @@ class Transfer
   attr_reader
 
   def initialize(sender, receiver, amount)
-    @sender
-    @receiver
+    @sender = sender
+    @receiver = receiver
     @status = "pending"
     @amount = amount
   end
