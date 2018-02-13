84d8611519b97390241b466341de71e2d7f94a52
Chely Ho <adept_inept@yahoo.com>
2018-02-13 01:31:29 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -14,7 +14,7 @@ class Transfer
   end
 
   def execute_transaction
-    if valid?
+    if valid? && status == "pending"
       @sender.balance -= amount
       receiver.deposit(amount)
       self.status = "complete"
