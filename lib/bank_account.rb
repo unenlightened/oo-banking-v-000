61a34fb1050243b32a7dbb477c55c84e8f6572ca
Chely Ho <adept_inept@yahoo.com>
2018-02-13 01:07:40 +0000

    automatically backed up by learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,10 +1,12 @@
 class BankAccount
-  attr_accessor
-  attr_reader
-  
+  attr_accessor :balance, :status
+  attr_reader :name
+
   def initialize(name, balance = 1000, status = open)
     @name = name
     @balance = balance
     @statu = status
   end
+
+
 end
